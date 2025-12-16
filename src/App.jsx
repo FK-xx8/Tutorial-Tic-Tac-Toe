6d8dbbbb089ce34642c48195a66ef626a251917c
Fuka KAWAMINAMI <f_kawaminami@avelio.co.jp>
2025-12-16 19:58:04 +0900

    押下済みでもボタンを押すとターン数が増えてしまうのを修正・わかりにくかったのでエラー文もどきを追加

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,41 @@ function App() {
   const [boards, setBoard] = useState(["1","2","3","4","5","6","7","8","9"]);
   // ターンの管理をする
   const [nowTurn, setnowTurn] = useState(0);
+  // エラー文の管理をする
+  const [error, setError] = useState(null);
 
+  // マスを押したときの処理
   const handleClickMasu = (index) => {
+    // 最大ターン数以上処理をしないようにする
     if (nowTurn === 9) {
-      alert("The game is already finished! Prease press 'Go to game start' button.");
+      setError("ゲームは終了しています！");
       return;
-    } else {
-      setnowTurn(turn => turn + 1);
     }
+
+    // マスが既に押されている場合は、以降の処理を行わないようにする
+    if (boards[index] == "X" || boards[index] == "O") {
+      setError("ズルはよくない！");
+      console.log("既に押下済み！");
+      return;
+    }
+
+    // ボードにXかOを出す
     setBoard(prev => {
       const spreadBoard = [...prev];
-      if (spreadBoard[index] !== "X" && spreadBoard[index] !== "O"){
-        spreadBoard[index] = nowTurn % 2 === 1 ? "X" : "O";
-        return spreadBoard;
-      }
+      // マスがまだ押されていない場合に処理を行なう
+      spreadBoard[index] = nowTurn % 2 === 1 ? "X" : "O";
       return spreadBoard;
     });
+
+    console.log("しっかり処理できた！")
+
+    // 経過ターン数を増やす
+    setnowTurn(turn => turn + 1);
+    // エラー文を削除する
+    setError("");
   };
 
+  // 次のプレイヤーが誰かを管理する
   const nextPlayer = nowTurn % 2 === 1 ? "X" : "O";
 
   return (
@@ -32,7 +49,6 @@ function App() {
       <div className="left-contents">
         <a>Next player: {nextPlayer}</a>
         <p>now turn count is {nowTurn}</p>
-        {}
         <div className="game-ground">
           {/* ここで繰り返し処理 */}
             {boards.map((board, index) => (
@@ -42,6 +58,7 @@ function App() {
                   value={board} />
             ))}
         </div>
+        <p className="error">{error}</p>
       </div>
       
       <div className="right-contents">
